2c7f6cfe2b70fe849f5c6b458444767013e9278d
zemosamurai <zemosamurai@gmail.com>
2022-11-28 18:33:26 +0300

    add stories

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,45 +1,45 @@
 import React, {useState} from 'react';
 import {Select} from "./Select";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Select',
     component: Select,
-
 };
 
-// const Template: Story<SelectPropsType> = (args) => <Select {...args} />;
-
-// const itemsArr = [
-//     {title: "Dimych", value: 1},
-//     {title: "Kim", value: 2},
-//     {title: "Denis", value: 3},
-//     {title: "Egor", value: 4}
-// ]
-
-// const onChangeCallBack = (id: any)=> {alert(id)}
-// const [value, setValue] = useState<>(1)
-//
-// export const SelectMode = Template.bind({});
-// SelectMode.args = {
-//     value: value,
-//     items: itemsArr,
-//     onChange: (id)=>onChangeCallBack(id)
-// };
-
-export const SelectMode = () => {
-    const [valueDef, setValueDef] = useState<string | null>('all')
+export const WithValue = () => {
+    const [value, setValue] = useState('2')
     const itemsArr = [
-        {title: "Dimych", value: 1},
-        {title: "Kim", value: 2},
-        {title: "Denis", value: 3},
-        {title: "Egor", value: 4}
+        {title: "Dimych", value: '1'},
+        {title: "Kim", value: '2'},
+        {title: "Denis", value: '3'},
+        {title: "Egor", value: '4'}
     ]
-    const onChangeCallBack = (id: any)=> {
-        let value = [...itemsArr].filter(el => el.value === id)
-        setValueDef(value[0].title)
-    }
 
     return (
-        <Select items={itemsArr} value={valueDef} onChange={onChangeCallBack}/>
+        <Select
+            items={itemsArr}
+            value={value}
+            onChange={setValue}
+        />
+    )
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null)
+
+    const itemsArr = [
+        {title: "Dimych", value: '1'},
+        {title: "Kim", value: '2'},
+        {title: "Denis", value: '3'},
+        {title: "Egor", value: '4'}
+    ]
+
+    return (
+        <Select
+            items={itemsArr}
+            value={value}
+            onChange={setValue}
+        />
     )
 }
