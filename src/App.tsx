5214235636ffae5f3d34925493493536175afc95
zemosamurai <zemosamurai@gmail.com>
2022-12-07 15:33:35 +0300

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Rating, RatingValueType} from "./components/Rating/Rating";
-import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
-import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
-import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
-import {Accordion} from "./components/Accordion/Accordion";
-import {OnOff} from "./components/OnOff/OnOff";
+import {RatingValueType} from "./components/Rating/Rating";
 
 function App() {
     let [ratingValue, setRatingValue] = useState<RatingValueType>(3)
@@ -15,19 +10,7 @@ function App() {
 
     return (
         <div className={'App'}>
-            {/*<PageTitle title={'This is APP component'}/>*/}
-            {/*<PageTitle title={'My Friends'}/>*/}
-            {/*<UncontrolledRating value={3}/>*/}
-            <Accordion titleValue={'Menu'} collapsed={accordionCollapsed} callback={setAccordionCollapsed}/>
-            {/*<Accordion titleValue={'Users'} collapsed={accordionCollapsed} callback={setAccordionCollapsed}/>*/}
-            {/*<Rating value={ratingValue} callback={setRatingValue}/>*/}
-
-            <UncontrolledOnOff onChange={setOnOffValue}/>
-            {/*<OnOff value={onOffValue} callback={setOnOffValue}/>*/}
-            {/*<UncontrolledAccordion titleValue={'Menu'}/>*/}
-            {/*<UncontrolledAccordion titleValue={'Users'}/>*/}
-            {/*<UncontrolledRating />*/}
-
+            <PageTitle title={'My Friends'}/>
         </div>
     );
 }
