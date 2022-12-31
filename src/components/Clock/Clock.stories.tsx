6787813a65cc2924082af4917f2d05d1a06c7708
zemosamurai <zemosamurai@gmail.com>
2022-12-31 15:00:17 +0300

    refactoring code

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -1,41 +1,16 @@
 import React from "react";
-import {Clock, ClockPropsType} from "./Clock";
-import {Story} from "@storybook/react";
+import {Clock} from "./Clock";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 export default {
-    title: 'Clock',
-    component: Clock
-}
+    title: ' Clock',
+    component: Clock,
 
-// export const BaseExample = () => {
-//     return <Clock mode={false}/>
-// }
+} as ComponentMeta<typeof Clock>;
 
-// export const SetTimeoutExample = () => {
-//     const [counter, setCounter] = useState<Date>(new Date(Date.now()))
-//     console.log('SetTimeoutExample')
-//
-//     useEffect(() => {
-//         setInterval(() => {
-//             setCounter(new Date(Date.now()))
-//             // setCounter((state: number) => state + 1)
-//         }, 1000)
-//     }, [])
-//
-//
-//     return (
-//         <>
-//             hello, counter: {counter.toLocaleTimeString('ru-Ru')}
-//             {/* <button onClick={() => setCounter(counter + 1)}>counter +</button>
-//             <button onClick={() => setFake(fake + 1)}>fake +</button>*/}
-//         </>
-//     )
-// }
+const Template: ComponentStory<typeof Clock> = (args) => <Clock {...args} />;
 
-
-const Template:  Story<ClockPropsType> = (args) => <Clock {...args} />;
-
-export const EditClock = Template.bind({});
-EditClock.args = {
-    mode: true
+export const ClockBase = Template.bind({});
+ClockBase.args = {
+   mode: 'digital'
 };
